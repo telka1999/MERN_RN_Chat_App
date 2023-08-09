964f814382bbe6476dac45cf191c69d279dacd42
bartosz <bartosz@bartosz1999.local>
2023-08-09 20:00:03 +0200

    Added mongodb conection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
