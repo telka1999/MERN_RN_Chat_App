e7e59f477174d037c2ca6a36ca17cf10577299c0
bartosz <bartosz@bartosz1999.local>
2023-08-10 16:38:46 +0200

    Fetching data from server to app

--- app/App.js
+++ app/App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const fetchData = async () => {
+    try {
+      const res = await fetch("http://10.0.2.2:5000/api/users/register", {
+        method: "POST",
+        redirect: "follow",
+      });
+      const data = await res.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>MERN RN Firebase Chat App</Text>
+      <Text>MERN RN Firebase Chat App data: {data?.message}</Text>
       <StatusBar style="auto" />
+      <Button onPress={fetchData} title="Click" />
     </View>
   );
 }
